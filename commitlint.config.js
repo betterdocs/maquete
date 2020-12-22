c3cf3270d0303db48d65c1c1896faacc345f9e43
Leonardo Cunha <leonardo.msc42@gmail.com>
2020-12-22 18:46:17 -0300

    refactor(commitlint): commitlint rules updated
    I think we don't need to use this issue rule anymore.
    Because, commitizen already has an option
    to attach an issue number.
    As we can see down below
    
    BREAKING CHANGE: scope-enum rule removed
    
    fix #21

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,6 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
-    rules: {
-        'scope-enum': [1, 'always', []]
-    },
+
     ignores: [
         (message) => message.includes('WIP')
     ]
